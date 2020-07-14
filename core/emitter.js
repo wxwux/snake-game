35ff8bdb13100d9f814776a843e89333c76ce619
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-07-14 11:06:24 +0300

    добавил эмиттер

---
+++ core/emitter.js
@@ -0,0 +1,24 @@
+export class Emitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(type, listener) {
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+  }
+
+  emit(type) {
+    if (this.events[type]) {
+      this.events[type].forEach((listener) => {
+        listener();
+      });
+    }
+  }
+}
+
+export const emitter = new Emitter();
+
+export const events = {
+  "SCORE": "SCORE" 
+}
